799231f4b885c78f9d9da0c575acd6a042f88628
Jz1nn <jz1nnwln@gmail.com>
2023-11-19 10:50:54 -0300

    Connected to the Firebase database

--- AcademiaRecyclerView/app/src/main/java/com/example/academiarecyclerview/Aluno.java
+++ AcademiaRecyclerView/app/src/main/java/com/example/academiarecyclerview/Aluno.java
@@ -1,22 +1,27 @@
 package com.example.academiarecyclerview;
 
+import java.util.Objects;
+
 public class Aluno {
+    // Construtor publico sem parametros (Realtime Database)
+    public Aluno(){
+    }
+
+    private String id;
     private String nome;
     private int idade;
     private double altura;
     private double peso;
     private String unidadeCadastro;
 
-    // Construtor
-    public Aluno(String nome, int idade, double altura, double peso, String unidadeCadastro) {
-        this.nome = nome;
-        this.idade = idade;
-        this.altura = altura;
-        this.peso = peso;
-        this.unidadeCadastro = unidadeCadastro;
+    public String getId() {
+        return id;
     }
 
-    // Métodos Getters e Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -53,6 +58,27 @@ public class Aluno {
         return unidadeCadastro;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(getId(), aluno.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    // Construtor
+    public Aluno(String nome, int idade, double altura, double peso, String unidadeCadastro) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.unidadeCadastro = unidadeCadastro;
+    }
     public void setUnidadeCadastro(String unidadeCadastro) {
         this.unidadeCadastro = unidadeCadastro;
     }
